2fc5df28df2fe94ab6c9ad449a073afbb7c03541
DanWBR <danielwag@gmail.com>
2019-09-29 09:53:36 -0400

    update

--- ReoGrid/Eto/ReoGridFullControl.cs
+++ ReoGrid/Eto/ReoGridFullControl.cs
@@ -1,4 +1,5 @@
 ﻿using Eto.Forms;
+using Eto.Drawing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,43 @@ using System.Threading.Tasks;
 
 namespace DWSIM.CrossPlatform.UI.Controls.ReoGrid
 {
-    class ReoGridFullControl: TableLayout
+    public class ReoGridFullControl: PixelLayout
     {
+
+        public ReoGridFullControl(): base()
+        {
+
+            var container = new DynamicLayout();
+
+            container.BeginVertical();
+
+            this.SizeChanged += (sender, e) => {
+                Console.WriteLine("PixelLayout Size Changed");
+                container.Size = new Size(this.Width, this.Height);
+            };
+
+            DWSIM.CrossPlatform.UI.Controls.ReoGrid.ReoGridControl rgcontrol;
+
+            rgcontrol = new DWSIM.CrossPlatform.UI.Controls.ReoGrid.ReoGridControl(this);
+            container.Add(rgcontrol, true, true);
+
+            rgcontrol.NewWorksheet();
+            rgcontrol.NewWorksheet();
+            rgcontrol.NewWorksheet();
+            rgcontrol.NewWorksheet();
+            rgcontrol.NewWorksheet();
+
+            this.Add(container, 0, 0);
+            this.Add(rgcontrol.editTextbox, 0, 0);
+
+            container.Add(rgcontrol.bottomPanel);
+
+            container.EndVertical();
+
+            rgcontrol.Width = 5000;
+            rgcontrol.Height = 2000;
+
+        }
 
     }
 }
--- ReoGridTest/Form.cs
+++ ReoGridTest/Form.cs
@@ -15,45 +15,11 @@ namespace ReoGridTest
         void Init()
         {
 
-            var pixlayout = new PixelLayout();
-
-            var container = new DynamicLayout();
-
-            container.BeginVertical();
-            
-            pixlayout.SizeChanged += (sender, e) => {
-                Console.WriteLine("PixelLayout Size Changed");
-                container.Size = new Size(pixlayout.Width, pixlayout.Height);
-            };
-
-            DWSIM.CrossPlatform.UI.Controls.ReoGrid.ReoGridControl rgcontrol;
-
-            rgcontrol = new DWSIM.CrossPlatform.UI.Controls.ReoGrid.ReoGridControl(pixlayout);
-            container.Add(rgcontrol, true, true);
-
-            rgcontrol.NewWorksheet();
-            rgcontrol.NewWorksheet();
-            rgcontrol.NewWorksheet();
-            rgcontrol.NewWorksheet();
-            rgcontrol.NewWorksheet();
-
-            pixlayout.Add(container, 0, 0);
-            pixlayout.Add(rgcontrol.editTextbox, 0, 0);
-
-            container.Add(rgcontrol.bottomPanel);
-
-            container.EndVertical();
-
-            rgcontrol.Width = 5000;
-            rgcontrol.Height = 2000;
-
             Title = "ReoGrid Eto Demo";
 
             ClientSize = new Size(800, 480);
 
-            pixlayout.Size = ClientSize;
-
-            Content = pixlayout;
+            Content = new DWSIM.CrossPlatform.UI.Controls.ReoGrid.ReoGridFullControl();
 
         }
 
